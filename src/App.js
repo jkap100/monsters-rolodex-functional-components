bb372077d4d690b9e8c574774cfd02a5ba16814e
Jon Kaplon <jon.kaplon1@gmail.com>
2022-08-30 09:41:04 -0700

    title

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./App.css";
 
 const App = () => {
   const [searchField, setSearchField] = useState("");
+  const [title, setTitle] = useState("Monsters Rolodex");
   const [monsters, setMonsters] = useState([]);
   const [filteredMonsters, setFilteredMonsters] = useState(monsters);
 
@@ -32,14 +33,26 @@ const App = () => {
     setSearchField(searchFieldString);
   };
 
+  const onTitleChange = (e) => {
+    const searchFieldString = e.target.value.toLowerCase();
+
+    setTitle(searchFieldString);
+  };
+
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">{title}</h1>
 
       <SearchBox
         onChangeHandler={onSearchChange}
         placeholder="search monsters"
         className="monsters search-box"
+      />
+      <br />
+      <SearchBox
+        onChangeHandler={onTitleChange}
+        placeholder="set title"
+        className="title search-box"
       />
       <CardList monsters={filteredMonsters} />
     </div>
